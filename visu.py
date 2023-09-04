5a7d54e91b2a12eac40b4d2e82afe1e43a6d0511
salwamohammed23 <140807017+salwamohammed23@users.noreply.github.com>
2023-09-05 01:20:00 +0300

    Update visu.py

--- visu.py
+++ visu.py
@@ -10,3 +10,26 @@ import pandas_profiling
 def perform_eda(data):
     profile = pandas_profiling.ProfileReport(data)
     return profile
+def main():
+    st.title("My PyCaret Package Web App")
+
+    # Data Loading
+    st.header("Data Loading")
+    file_path = st.file_uploader("Upload CSV file", type="csv")
+    if file_path is not None:
+        data = load_data(file_path)
+        st.success("Data loaded successfully!")
+    else:
+        st.warning("Please upload a CSV file.")
+
+    # EDA
+    st.header("Exploratory Data Analysis (EDA)")
+    if st.button("Perform EDA"):
+        profile = perform_eda(data)
+        st_profile_report(profile)
+
+  
+
+
+if __name__ == "__main__":
+    main()    
