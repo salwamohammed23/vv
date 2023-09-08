be6e2d2d5bea7ea97b48f1e6d4e48de71589746d
salwamohammed23 <140807017+salwamohammed23@users.noreply.github.com>
2023-09-08 23:30:57 +0300

    Update Build_Your_Own_PyCaret.py

--- Build_Your_Own_PyCaret.py
+++ Build_Your_Own_PyCaret.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import streamlit as st
-
+from pycaret.classification import *
+from pycaret.regression import *
 from sklearn.model_selection import train_test_split
 from pycaret.datasets import get_data
 from sklearn.metrics import mean_squared_error, accuracy_score
