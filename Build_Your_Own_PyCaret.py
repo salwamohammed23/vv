c138dad980f3646eaedebdc137b5923dfac9ebe2
salwamohammed23 <140807017+salwamohammed23@users.noreply.github.com>
2023-09-08 18:07:16 +0300

    Update Build_Your_Own_PyCaret.py

--- Build_Your_Own_PyCaret.py
+++ Build_Your_Own_PyCaret.py
@@ -66,9 +66,6 @@ def main():
         # Split data into training and testing sets
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-
-        )
-
         # Select models
         models = {}
 
