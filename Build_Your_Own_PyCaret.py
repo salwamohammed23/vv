b37fef5cd78174c4007ccb87de5980b8c06ec8f6
salwamohammed23 <140807017+salwamohammed23@users.noreply.github.com>
2023-09-08 19:50:40 +0300

    Update Build_Your_Own_PyCaret.py

--- Build_Your_Own_PyCaret.py
+++ Build_Your_Own_PyCaret.py
@@ -70,15 +70,14 @@ def main():
         models = {}
 
         model_type = st.radio("Select the model type", ("Regression", "Classification"))
+        best_model=compare_models()
         
         if model_type == 'Regression':
-            best_model=compare_models()
             st.text(print(best_model))
             selected_models = st.multiselect("Select models", ["lr", "rf", "xgboost"])
             models.update(selected_models)
 
         if model_type == 'Classification':
-            best_model=compare_models()
             st.text(print(best_model))
             selected_models = st.multiselect("Select models", ["lrc", "rfc", "xgboostc"])
             models.update(selected_models)
