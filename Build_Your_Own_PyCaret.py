d24b5ca085044785c40627cfd500f7cd3cfa224c
salwamohammed23 <140807017+salwamohammed23@users.noreply.github.com>
2023-09-10 22:48:06 +0300

    Update Build_Your_Own_PyCaret.py

--- Build_Your_Own_PyCaret.py
+++ Build_Your_Own_PyCaret.py
@@ -10,6 +10,7 @@ from sklearn.metrics import mean_squared_error, accuracy_score
 from pycaret.regression import compare_models, setup, create_model as create_regression_model, finalize_model
 from pycaret.classification import compare_models as compare_class_models
 from pycaret.classification import setup, create_model as create_classification, finalize_model
+from pycaret.regression import setup, create_model, finalize_model
 
 # Function to load data
 def load_data(file):
