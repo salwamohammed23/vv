2f7cece7978e7bf159b9283f1de0aca3e4458627
salwamohammed23 <140807017+salwamohammed23@users.noreply.github.com>
2023-09-10 22:52:36 +0300

    Update Build_Your_Own_PyCaret.py

--- Build_Your_Own_PyCaret.py
+++ Build_Your_Own_PyCaret.py
@@ -33,20 +33,25 @@ def train_models(X_train, y_train, model_type, selected_models):
     trained_models = {}
 
     if model_type == 'Classification':
-        # Set up the classification problem
         clf = setup(data=X_train, target=y_train)
         
         for model_name in selected_models:
-            model = create_model(model_name, fold=5)
-            trained_model = finalize_model(model)
-            trained_models[model_name] = trained_model
+            try:
+                model = create_model(model_name, fold=5)
+                trained_model = finalize_model(model)
+                trained_models[model_name] = trained_model
+            except ValueError as e:
+                st.error(f"Error training model {model_name}: {str(e)}")
 
     else:
         reg = setup(data=X_train, target=y_train)
         for model_name in selected_models:
-            model = create_model(model_name)
-            trained_model = finalize_model(model)
-            trained_models[model_name] = trained_model
+            try:
+                model = create_model(model_name)
+                trained_model = finalize_model(model)
+                trained_models[model_name] = trained_model
+            except ValueError as e:
+                st.error(f"Error training model {model_name}: {str(e)}")
 
     return trained_models
 
