212c7d753895e44947f555f4aad41ffdb40980cc
salwamohammed23 <140807017+salwamohammed23@users.noreply.github.com>
2023-09-13 19:49:14 +0300

    Update test.py

--- test.py
+++ test.py
@@ -95,10 +95,12 @@ def main():
     # Upload data
     st.sidebar.subheader('Data Loading')
     file = st.sidebar.file_uploader('Upload CSV', type='csv')
-    st.write(data.head())
+   
  
     if file is not None:
         data = wrangle(file)  # Assuming you have a function named `wrangle` that processes the uploaded file
+                
+        st.write(data.head())
         # Select target variable
         target_variable = st.sidebar.selectbox('Select the target variable', data.columns)
         
