fe67b05ea6583292cdc424bae62dd1e95b399c3c
salwamohammed23 <140807017+salwamohammed23@users.noreply.github.com>
2023-09-14 13:47:19 +0300

    Update test.py

--- test.py
+++ test.py
@@ -20,7 +20,7 @@ import streamlit as st
 def wrangle(filepath):
     # Function to load data from different formats
     def load_data(file_path):
-        _, file_extension = file_path.rsplit('.', 1)
+        #_, file_extension = file_path.rsplit('.', 1)
 
         if file_extension == 'csv':
             return pd.read_csv(file_path)
