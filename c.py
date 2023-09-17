e1d1972071af15de008a079aa030c8a3196e66c3
salwamohammed23 <140807017+salwamohammed23@users.noreply.github.com>
2023-09-17 04:20:45 +0300

    Update c.py

--- c.py
+++ c.py
@@ -1,89 +1,236 @@
 import pandas as pd
-import streamlit as st
-from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas_profiling as pf
+from streamlit_pandas_profiling import st_profile_report
 from pycaret.classification import *
 from pycaret.regression import *
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import train_test_split
+from pycaret.datasets import get_data
 from sklearn.metrics import mean_squared_error, accuracy_score
+import streamlit as st
+#st.set_option('deprecation.showPyplotGlobalUse', False
+
+###############################################################################################################
 
 # Function to load data
-def load_data(file):
-    data = pd.read_csv(file)
+# Step 1: Data Cleaning
+#Data Quality: The data cleaning steps, such as handling missing values and removing duplicates,
+# ensure data quality and improve the reliability of the analysis. This indicates a concern for accurate and reliable insights.
+
+def wrangle(filepath, file_format):
+    # Function to load data from different formats
+    def load_data(file_path, file_format):
+        if file_format == 'csv':
+            return pd.read_csv(file_path)
+        elif file_format == 'excel':
+            return pd.read_excel(file_path)
+        elif file_format == 'sql':
+            # Code to load data from SQL database
+            #conn = sqlite3.connect('database.db')
+            pass
+
+    # Load data using the load_data function
+    data = load_data(filepath, file_format)
+
+
+
+    duplicate_values = data.duplicated().sum()
+
+    if duplicate_values > 0:
+        # Function to handle duplicate values
+        def handle_duplicate_values(df):
+            # Remove duplicate rows
+            df.drop_duplicates(inplace=True)
+            return df
+
+        data = handle_duplicate_values(data)
+
     return data
-    
-def train_models(X_train, y_train, model_type, selected_models):
+   ############################################################################################### 
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder
+
+def handle_Normalize_missing_values(data):
+    continuous_features = []
+    categorical_features = []
+
+    # Identify the type of each feature in the data
+    for column in data.columns:
+        if data[column].dtype == 'object' or data[column].nunique() <= 10:
+            categorical_features.append(column)
+        else:
+            continuous_features.append(column)
+
+    # Fill missing values in categorical features with the mode
+    for feature in categorical_features:
+        data[feature].fillna(data[feature].mode()[0], inplace=True)
+
+    # Fill missing values in continuous features with the mean
+    for feature in continuous_features:
+        data[feature].fillna(data[feature].mean(), inplace=True)
+
+    # Normalize continuous features
+    scaler = MinMaxScaler()
+    data[continuous_features] = scaler.fit_transform(data[continuous_features])
+
+    # Encode categorical features
+    encoder = LabelEncoder()
+    for feature in categorical_features:
+        data[feature] = encoder.fit_transform(data[feature])
+
+    return data
+    ########################################################################################
+
+
+
+def generate_histograms(df):
+    for col in df.select_dtypes(include='number'):
+        plt.figure()
+        fig, ax = plt.subplots()
+        ax =sns.histplot(df[col])
+        plt.title(f'Histogram of {col}')
+        # ... Perform your plotting actions on the figure ...
+        st.pyplot(fig)
+
+# Function to generate box plots
+def generate_box_plots(df):
+    for col in df.select_dtypes(include='number'):
+        plt.figure()
+        fig, ax = plt.subplots()
+        ax =sns.boxplot(data=df[col])
+        plt.title(f'Box Plot of {col}')
+        # After
+         
+        # ... Perform your plotting actions on the figure ...
+        st.pyplot(fig)
+
+# Function to generate scatter plots
+def generate_scatter_plots(df):
+    numerical_cols = df.select_dtypes(include='number').columns
+
+    for i, col1 in enumerate(numerical_cols):
+        for j, col2 in enumerate(numerical_cols):
+            if i < j:
+                plt.figure()
+                fig, ax = plt.subplots()
+                ax = sns.scatterplot(data=df, x=col1, y=col2)
+                plt.title(f'Scatter Plot of {col1} vs {col2}')
+                # After
+                 
+                # ... Perform your plotting actions on the figure ...
+                st.pyplot(fig)
+                
+##############################################################################################################
+
+from pycaret.classification import ClassificationExperiment
+from pycaret.regression import RegressionExperiment
+
+def train_validate_models(X_train, y_train, X_test, y_test, model_type):
     trained_models = {}
-
-    if model_type == 'Classification':
-        # Set up the classification problem
-        clf = setup(data=X_train, target=y_train)
-        
-        for model_name in selected_models:
-            model = create_model(model_name, fold=5)
-            trained_model = finalize_model(model)
-            trained_models[model_name] = trained_model
-
-    else:
-        reg = setup(data=X_train, target=y_train)
-        for model_name in selected_models:
-            model = create_model(model_name)
-            trained_model = finalize_model(model)
-            trained_models[model_name] = trained_model
-
-    return trained_models
-
-# Function to evaluate models
-def evaluate_models(X_test, y_test, trained_models):
     scores = {}
 
-    for model_name, model in trained_models.items():
-        if 'Regression' in model_name:
-            y_pred = predict_model(model, data=X_test)
-            score = mean_squared_error(y_test, y_pred)
-        else:
-            y_pred = predict_model(model, data=X_test)
-            score = accuracy_score(y_test, y_pred)
+    if model_type == 'Classification':
+        try:
+            setup(data=X_train, target=y_train)
+            exp = ClassificationExperiment()
+            exp.setup(data=X_train, target=y_train)  # Replace 'target_column_name' with the actual name of your target column
+            best=exp.compare_models()
+            eval=evaluate_model(best)
 
-        scores[model_name] = score
+        except Exception as e:
+            print(f"An error occurred during classification model training: {str(e)}")
 
-    return scores
+    elif model_type == 'Regression':
+        try:
+            setup(data=X_train, target=y_train)
+            exp = RegressionExperiment()
+            exp.setup(data=X_train, target=y_train)  # Replace 'target_column_name' with the actual name of your target column
+            best=exp.compare_models()
+            eval=evaluate_model(best)
 
+        except Exception as e:
+            print(f"An error occurred during regression model training: {str(e)}")
+
+    else:
+        print("Invalid model type. Please choose either 'Classification' or 'Regression'.")
+
+    return eval, best
+    ############################################################################################
 def main():
+    
     st.sidebar.title('Machine Learning Package')
 
     # Upload data
-    st.sidebar.subheader('Data Loading')
-    file = st.sidebar.file_uploader('Upload CSV', type='csv')
+    st.sidebar.subheader("File Selection")
 
-    if file is not None:
-        data = load_data(file)
+    # File format selection
+    file_format = st.sidebar.selectbox("Select File Format", ['csv', 'excel'])
+
+    # File upload
+    filepath = st.sidebar.file_uploader("Upload File", type=[file_format])
+
+    if filepath is not None:
+        try:
+            data = wrangle(filepath, file_format)
+            data= handle_Normalize_missing_values(data)
+            #st.dataframe(data)  # Display loaded dataset
+        except Exception as e:
+            st.error(f"Error: {str(e)}")
+
         st.sidebar.success('Data successfully loaded!')
         st.write(data.head())
-
         # Select target variable
         target_variable = st.sidebar.selectbox('Select the target variable', data.columns)
-
+        
+###############################################################################################       
+        # Select columns_need
+        columns_need = st.sidebar.multiselect('Select the columns which you need', data.columns)
+        
+        if st.button('Select_columns_need'):
+            data=data[columns_need]
+            #st.write(data.head())
+            st.write(data.columns)
         # Check if data is empty
-        if data.empty:
-            st.error('The uploaded data is empty.')
-            return
-
+###################################################################################
         # Split data into features and target
         X = data.drop(target_variable, axis=1)
         y = data[target_variable]
-
-        # Display a title
+###########################################################################################
+          # Display a title
         st.title('Perform EDA')
 
         # Display EDA
         st.subheader('Exploratory Data Analysis')
-        if st.sidebar.button('Generate EDA'):
+        if st.button('Generate EDA'):
             if X.empty:
                 st.error('The feature data is empty.')
                 return
             else:
-                profile_report = data.profile_report()
-                st_profile_report(profile_report)
-
+                # Generate histograms
+                st.header("Histograms")
+                generate_histograms(data)
+                
+                # Generate box plots
+                st.header("Box Plots")
+                generate_box_plots(data)
+                
+                # Generate scatter plots
+                st.header("Scatter Plots")
+                generate_scatter_plots(data)
+#############################################################################################
+                # Display Statistical
+               # Generate summary statistics
+        st.subheader('Summary Statistics')
+        if st.button('Generate Summary Statistics'):
+            summary_stats = data.describe()
+            st.write(summary_stats)
+                    # Calculate mode
+            st.subheader('Mode')
+            #if st.button('Calculate Mode'):
+            mode = data.mode().iloc[0]
+            st.write(mode)
+            ###############################################################################3
         # Split data into training and testing sets
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
@@ -92,30 +239,19 @@ def main():
             st.error('The training data is empty.')
             return
 
-        # Select models
+            # Select models
         models = {}
 
         model_type = st.radio("Select the model type", ("Regression", "Classification"))
 
         if model_type == 'Regression':
-            selected_models = st.multiselect("Select models", ['gbr', 'lightgbm', 'xgboost', 'lar', 'llar', 'et', 'en', 'lasso', 'knn', 
-                                                                  'ada', 'omp', 'par', 'huber', 'dt', 'lr', 'br', 'rf', 'ridge', 'dummy'])
-            models.update({model: True for model in selected_models})
+            train_validate_models(X_train, y_train, X_test, y_test, model_type)
 
         if model_type == 'Classification':
-            selected_models = st.select("Select models", ['gbc', 'lightgbm', 'ada', 'xgboost', 'lda', 'et', 'gbc', 'ada', 'knn', 'lr', 'rf', 'ridge', 'dummy'])
-            models.update({model: True for model in selected_models})
-
-        if st.sidebar.button('Train Models'):
-            trained_models =train_models(X_train, y_train, model_type, selected_models)
-            st.success('Models trained successfully!')
-
-            # Evaluate models
-            scores = evaluate_models(X_test, y_test, trained_models)
-            st.subheader('Model Evaluation')
-
-            for model_name, score in scores.items():
-                st.write(f'{model_name}: {score}')
-
+            train_validate_models(X_train, y_train, X_test, y_test, model_type)
+        if st.button('Train Models'):
+            st.write( best)
+            st.write(eval)
+       
 if __name__ == '__main__':
     main()
