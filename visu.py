cbda246fd430e61bc66352b23ed5a0235cf7732c
salwamohammed23 <140807017+salwamohammed23@users.noreply.github.com>
2023-10-09 18:57:28 +0300

    Update visu.py

--- visu.py
+++ visu.py
@@ -1,24 +1,15 @@
 import pandas as pd
 import streamlit as st
 import pandas_profiling as pf
-from streamlit_pandas_profiling import st_profile_report
-from pycaret.classification import *
-from pycaret.regression import *
-from sklearn.model_selection import train_test_split
-from pycaret.datasets import get_data
-from sklearn.metrics import mean_squared_error, accuracy_score
-
+from sklearn.impute import SimpleImputer
 
 
 # Function to load data
-# Step 1: Data Cleaning
-#Data Quality: The data cleaning steps, such as handling missing values and removing duplicates,
-# ensure data quality and improve the reliability of the analysis. This indicates a concern for accurate and reliable insights.
 def wrangle(filepath):
     # Read CSV file
     data = pd.read_csv(filepath)
-    null_sum = data.isnull().sum()
 
+    null_sum = data.isnull().sum()
     if null_sum.sum() > 0:
         # Function to handle missing values
         def handle_missing_values(df):
@@ -29,12 +20,9 @@ def wrangle(filepath):
 
         data = handle_missing_values(data)
 
-
-
     duplicate_values = data.duplicated().sum()
-
     if duplicate_values.sum() > 0:
-        # Function to handle missing values
+        # Function to handle duplicate values
         def handle_duplicate_values(data):
             # Remove duplicate rows
             data.drop_duplicates(inplace=True)
@@ -42,19 +30,8 @@ def wrangle(filepath):
 
         data = handle_duplicate_values(data)
 
-
-
     return data
-    
-# Init setup
-#s = setup(data, target=target_variable, session_id=123)
 
-@st.cache
-def generate_eda(data, target_variable):
-
-    s = setup(data=data, target=target_variable, session_id=123)
-    eda_output = eda()
-    return eda_output
 
 def main():
     st.sidebar.title('Machine Learning Package')
@@ -91,6 +68,8 @@ def main():
                 return
             else:
                 profile_report = data.profile_report()
-                st_profile_report(profile_report)
-            #eda_output = generate_eda(data, target_variable)
-            #st.write(eda_output[0])
+                st.write(profile_report.to_html())
+
+
+if __name__ == "__main__":
+    main()
