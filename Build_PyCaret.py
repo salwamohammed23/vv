359663ed026e96148a9b09e4e922dbfbf3dac2a7
salwamohammed23 <140807017+salwamohammed23@users.noreply.github.com>
2023-11-22 00:07:19 +0200

    Update Build_PyCaret.py

--- Build_PyCaret.py
+++ Build_PyCaret.py
@@ -11,59 +11,26 @@ import pycaret
 from pycaret.classification import setup, compare_models
 from pycaret.regression import setup as regression_setup, compare_models as regression_compare_models
 # functin to load_data
-def wrangle(filepath, file_format):
-    # Function to load data from different formats
-    def load_data(file_path, file_format):
-        if file_format == 'csv':
-            return pd.read_csv(file_path)
-        elif file_format == 'excel':
-            return pd.read_excel(file_path)
-        elif file_format == 'sql':
-            # Code to load data from SQL database
-            #conn = sqlite3.connect('database.db')
-            pass
+def load_data(file_path):
+    _, file_extension = os.path.splitext(file_path.name)
 
-    # Load data using the load_data function
-    data = load_data(filepath, file_format)
+    if file_extension == '.csv':
+        return pd.read_csv(file_path)
+    elif file_extension == '.xlsx':
+        return pd.read_excel(file_path)
+    #elif file_extension == 'sql':
+       # conn = sqlite3.connect('database.db')
+        # TODO: Implement SQL data loading logic
+        pass
+    else:
+        raise ValueError('Unsupported file format.')
 
-
-
-    duplicate_values = data.duplicated().sum()
-
-    if duplicate_values > 0:
-        # Function to handle duplicate values
-        def handle_duplicate_values(df):
-            # Remove duplicate rows
-            df.drop_duplicates(inplace=True)
-            return df
-
-        data = handle_duplicate_values(data)
-
+def handle_duplicate_values(data):
+    # Function to handle duplicate values
+    data.drop_duplicates(inplace=True)
     return data
-######################################################################
-#vigulize he data
-def generate_histograms(data):
-    for col in data.select_dtypes(include='number'):
-        st.pyplot(sns.histplot(data[col]))
-        st.title(f'Histogram of {col}')
 
-def generate_box_plots(data):
-    for col in data.select_dtypes(include='number'):
-        st.pyplot(sns.boxplot(data=data[col]))
-        st.title(f'Box Plot of {col}')
-
-def generate_scatter_plots(data):
-    numerical_cols = data.select_dtypes(include='number').columns
-
-    for i, col1 in enumerate(numerical_cols):
-        for j, col2 in enumerate(numerical_cols):
-            if i < j:
-                st.pyplot(sns.scatterplot(data=data, x=col1, y=col2))
-                st.title(f'Scatter Plot of {col1} vs {col2}')
-########################################################################
-#handle_Normalize_missing_values
-##################################################################################3
-def handle_Normalize_missing_values(data, categorical_features_tdeal, continuous_features_tdeal):
+def handle_normalize_missing_values(data, categorical_features_tdeal, continuous_features_tdeal):
     continuous_features = []
     categorical_features = []
 
@@ -100,6 +67,42 @@ def handle_Normalize_missing_values(data, categorical_features_tdeal, continuous
         data[feature] = encoder.fit_transform(data[feature])
 
     return data
+
+def wrangle(file_path, categorical_features_tdeal, continuous_features_tdeal):
+    data = load_data(file_path)
+    st.write('sum of nul value befor')
+    st.write(data.isnull().sum())
+    st.write('--------------------------------------------------------------------------------------------')
+    data =handle_duplicate_values(data)
+    data = handle_normalize_missing_values(data, categorical_features_tdeal, continuous_features_tdeal)
+    st.write('sum of nul value after')
+
+    st.write(data.isnull().sum())
+    return dat
+######################################################################
+#vigulize he data
+def generate_histograms(data):
+    for col in data.select_dtypes(include='number'):
+        st.pyplot(sns.histplot(data[col]))
+        st.title(f'Histogram of {col}')
+
+def generate_box_plots(data):
+    for col in data.select_dtypes(include='number'):
+        st.pyplot(sns.boxplot(data=data[col]))
+        st.title(f'Box Plot of {col}')
+
+def generate_scatter_plots(data):
+    numerical_cols = data.select_dtypes(include='number').columns
+
+    for i, col1 in enumerate(numerical_cols):
+        for j, col2 in enumerate(numerical_cols):
+            if i < j:
+                st.pyplot(sns.scatterplot(data=data, x=col1, y=col2))
+                st.title(f'Scatter Plot of {col1} vs {col2}')
+########################################################################
+#handle_Normalize_missing_values
+##################################################################################3
+
 #####################################################333
 #train_validate_models
 ######################################################
@@ -132,6 +135,8 @@ def train_validate_models(data, target_variable):
 #apply sttreamlit
 ################################################333333333
 def main():
+    continuous_features_tdeal  = st.sidebar.selectbox('choose the way to treat continuous features :',  ["mean()", "median()", "mode()"])
+    categorical_features_tdeal = st.sidebar.selectbox("choose the way to treat categorical features  :",  ["ordinal_encoder"])
     st.title('Machine Learning Package')
 
     # Load data
@@ -145,44 +150,34 @@ def main():
 
     if filepath is not None:
         try:
-            data = wrangle(filepath, file_format)
+            data = wrangle(file_path, categorical_features_tdeal, continuous_features_tdeal)
             # Perform other preprocessing steps as needed
-        except Exception as e:
-            st.error(f"Error: {str(e)}")
+
 
         st.sidebar.success('Data successfully loaded!')
         st.write(data.head())
      
 
     #####################################################################
-    User_choice = input("Do you want to drop column choose 'y' or 'n': ")
+    User_choice = st.radio("Do you want to explore your data?", ('Yes', 'No'))
     
     # Declare data here
-    if User_choice == 'y':
+    if User_choice == 'Yes':
         columns_to_drop = st.sidebar.multiselect('Select the columns you want to drop', data.columns)
         data.drop(columns=columns_to_drop, inplace=True)
-    elif User_choice == 'n':
+    elif User_choice == 'No':
         pass
-    else:
-        print("Invalid choice. Please choose 'y' or 'n'.")
-        return
 
-    print(data.columns)
+    
 
     # Show selected columns
     st.write(data.columns)
        ##############################################################################################################################
-    continuous_features_tdeal = input("choose the way to treat continuous features choose 'mean()', 'median()', or 'mode(): ")
-    categorical_features_tdeal = input("choose the way to treat continuous features choose 'ordinal_encoder', or 'imputer': ")
-        # Initialize the data variable
-    data = handle_Normalize_missing_values(data, categorical_features_tdeal, continuous_features_tdeal)
-        ########################################################################################
-
     # Select target variable
     target_variable = st.sidebar.selectbox('Select the target variable', data.columns)
 
     # Exploratory Data Analysis
-    exploratory_data_analysis = st.radio("Do you want to explore your data?", ('Yes', 'No'))
+    exploratory_data_analysis = st.radio("Do you want to explore your data?", ('No','Yes'))
     if exploratory_data_analysis == 'Yes':
             # Generate histograms
         generate_histograms(data)
@@ -204,6 +199,9 @@ def main():
     train_models = st.radio("Do you want to train models?", ('Yes', 'No'))
     if train_models == 'Yes':
         train_validate_models(data, target_variable)
+        except Exception as e:
+            st.error(f"Error: {str(e)}")
+        
 
 if __name__ == '__main__':
     main()
